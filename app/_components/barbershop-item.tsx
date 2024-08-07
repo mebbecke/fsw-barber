185618f855750fa6be71aebe686ae5ecfe6238b0
Marina Ebbecke <m.ebbecke@gmail.com>
2024-08-06 22:15:20 -0300

    feat: adds BarbershopItem component

---
+++ app/_components/barbershop-item.tsx
@@ -0,0 +1,42 @@
+import { Barbershop } from "@prisma/client"
+import { Card, CardContent } from "./ui/card"
+import Imagem from "next/image"
+import { Button } from "./ui/button"
+import { StarIcon } from "lucide-react"
+import { Badge } from "./ui/badge"
+
+interface BarbershopItemProps {
+  barbershop: Barbershop
+}
+
+export const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  return (
+    <Card className="min-w-[167px] rounded-2xl">
+      <CardContent className="p-0 px-1 pb-2 pt-1">
+        {/* IMAGEM */}
+        <div className="relative h-[159px] w-full">
+          <Imagem
+            alt={barbershop.name}
+            fill
+            className="rounded-2xl object-cover"
+            src={barbershop.imageUrl}
+          />
+
+          <Badge className="absolute left-2 top-2 gap-1" variant="secondary">
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="text-xs font-semibold">5,0</p>
+          </Badge>
+        </div>
+
+        {/* TEXTO */}
+        <div className="px-1 py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
